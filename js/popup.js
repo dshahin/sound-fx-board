ad06efed645796fdcd0779881fb4048aa87f5bc3
Dan Shahin <dan@codescience.com>
2016-03-27 14:26:48 -0700

    add tts options

--- js/popup.js
+++ js/popup.js
@@ -1,76 +1,213 @@
+var voiceOptions = {
+    //voiceName: 'Google UK English Male',
+    enqueue: false,
+    pitch: 1,
+    rate: 1,
+    volume: 1
+};
+
+var showSecret = false;
+
 chrome.storage.sync.get(function(prefs) {
     console.log('stored prefs:', prefs);
-    if(prefs.showSecret){
+    showSecret = prefs.showSecret;
+    if (showSecret) {
+
         $('#secret').show();
     }
-  
+
+    if(prefs.voiceOptions){
+        voiceOptions = prefs.voiceOptions;
+        $("#voicePitch").val(voiceOptions.pitch);
+        $('#voicePitchSlider').slider({ value: voiceOptions.pitch });
+        $("#voiceRate").val(voiceOptions.rate);
+        $('#voiceRateSlider').slider({ value: voiceOptions.rate });
+    }
+
 });
+
+chrome.tts.getVoices(
+    function(voices) {
+        voiceNames = [];
+        for (var i = 0; i < voices.length; i++) {
+            var vName = voices[i].voiceName;
+            voiceNames.push(vName);
+            if (vName === voiceOptions.voiceName) {
+                $('#voiceName').append('<option selected="selected" value="' + vName + '">' + vName + '</option>');
+            } else {
+                $('#voiceName').append('<option value="' + vName + '">' + vName + '</option>');
+            }
+
+        }
+    });
+
+
+
+var say = function(quote) {
+    chrome.tts.stop();
+    chrome.tts.speak(quote, voiceOptions);
+
+};
+
+var saveVoiceOptions = function(){
+
+    var prefs = {
+        voiceOptions : voiceOptions,
+        showSecret : showSecret
+    };
+
+    chrome.storage.sync.set(prefs, function() {
+        console.log('saving prefs:',prefs);
+    });
+};
 
 
 jQuery(document).ready(function($) {
-    var sounds =  {
+    var sounds = {
 
-        doorbell : {label:"Doorbell", fx:new Audio("/sounds/doorbell.ogg")},
-        phone : {label:"Phone", fx:new Audio("/sounds/cellring.ogg")},
-        crash : {label:"Crash", fx:new Audio("/sounds/237375_squareal_car-crash.ogg")},
+        doorbell: { label: "Doorbell", fx: new Audio("/sounds/doorbell.ogg") },
+        phone: { label: "Phone", fx: new Audio("/sounds/cellring.ogg") },
+        crash: { label: "Crash", fx: new Audio("/sounds/237375_squareal_car-crash.ogg") },
 
-        dive : {label:"Dive", fx:new Audio("/sounds/104882__cgeffex__submarie-dive-horn-better.wav")},
+        dive: { label: "Dive", fx: new Audio("/sounds/104882__cgeffex__submarie-dive-horn-better.wav") },
 
-        pop : {label:"Pop", fx:new Audio("/sounds/pop.ogg")},
-        ding : {label:"Ding", fx:new Audio("/sounds/ding.ogg")},
-        buzz : {label:"Buzz", fx:new Audio("/sounds/buzz.ogg")},
-        correct : {label:"Correct", fx:new Audio("/sounds/131660__bertrof__game-sound-correct.wav")},
+        pop: { label: "Pop", fx: new Audio("/sounds/pop.ogg") },
+        ding: { label: "Ding", fx: new Audio("/sounds/ding.ogg") },
+        buzz: { label: "Buzz", fx: new Audio("/sounds/buzz.ogg") },
+        correct: { label: "Correct", fx: new Audio("/sounds/131660__bertrof__game-sound-correct.wav") },
 
-        fart : {label:"Fart", fx:new Audio("/sounds/249583__ycbcr__short-definite-fart.wav")},
-        boo : {label:"Boo", fx:new Audio("/sounds/boo.ogg")},
-        cricket : { label:"Crickets", fx:new Audio("/sounds/cricket.ogg")},
-        rooster : { label:"Rooster", fx:new Audio("/sounds/166745_cocoricosound_cock-a-doodle-doo.ogg")},
-        laughs : {label:"Laughs", fx:new Audio("/sounds/laugh.ogg")},
-        price : {label:"The Price Is Wrong", fx:new Audio("/sounds/horns.ogg")},
-        actlikeaman : {label:"Act like a man", fx:new Audio("/sounds/actlikeaman.ogg")},
+        fart: { label: "Fart", fx: new Audio("/sounds/249583__ycbcr__short-definite-fart.wav") },
+        boo: { label: "Boo", fx: new Audio("/sounds/boo.ogg") },
+        cricket: { label: "Crickets", fx: new Audio("/sounds/cricket.ogg") },
+        rooster: { label: "Rooster", fx: new Audio("/sounds/166745_cocoricosound_cock-a-doodle-doo.ogg") },
+        laughs: { label: "Laughs", fx: new Audio("/sounds/laugh.ogg") },
+        price: { label: "The Price Is Wrong", fx: new Audio("/sounds/horns.ogg") },
+        actlikeaman: { label: "Act like a man", fx: new Audio("/sounds/actlikeaman.ogg") },
 
-        r2d2 : {label:"R2-D2", fx:new Audio("/sounds/r2d2.ogg")},
-        youblewit : { label:"You Blew It", fx:new Audio("/sounds/youblewit.ogg")},
-        khan : {label:"Khan!", fx:new Audio("/sounds/khan.ogg")},
-        cashregister : {label:"Cha-Ching", fx:new Audio("/sounds/cashregister.ogg")},
+        r2d2: { label: "R2-D2", fx: new Audio("/sounds/r2d2.ogg") },
+        youblewit: { label: "You Blew It", fx: new Audio("/sounds/youblewit.ogg") },
+        khan: { label: "Khan!", fx: new Audio("/sounds/khan.ogg") },
+        cashregister: { label: "Cha-Ching", fx: new Audio("/sounds/cashregister.ogg") },
 
-        pity : {label:"Mr. T", fx:new Audio("/sounds/pity.ogg")},    
-        yay : {label:"Yay!", fx:new Audio("/sounds/yay.ogg")},
-        alert : {label:"Red Alert", fx:new Audio("/sounds/alert.wav")},
-        flint : {label:"Our Man Flint", fx:new Audio("/sounds/flint.wav")}
+        pity: { label: "Mr. T", fx: new Audio("/sounds/pity.ogg") },
+        yay: { label: "Yay!", fx: new Audio("/sounds/yay.ogg") },
+        alert: { label: "Red Alert", fx: new Audio("/sounds/alert.wav") },
+        flint: { label: "Our Man Flint", fx: new Audio("/sounds/flint.wav") }
 
     };
 
     var secretSounds = {
-        shinebox : {label:"Shinebox", fx:new Audio("/sounds/shinebox.ogg")},
-        myname : {label:"My Name?", fx:new Audio("/sounds/myname.ogg")},
-        getlaid : {label:"Hey Everybody", fx:new Audio("/sounds/getlaid.ogg")}
+        shinebox: { label: "Shinebox", fx: new Audio("/sounds/shinebox.ogg") },
+        myname: { label: "My Name?", fx: new Audio("/sounds/myname.ogg") },
+        getlaid: { label: "Hey Everybody", fx: new Audio("/sounds/getlaid.ogg") }
     };
 
-    function makeButtons(sounds, parentId){
+    function makeButtons(sounds, parentId) {
 
-        for(var s in sounds){
+        for (var s in sounds) {
 
             var $button = $('#buttonTemplate').clone();
             var $sfx = jQuery.extend({}, sounds[s]);
 
-            var  playSound = function(){
-            var $button = $(this),
-              $snd = $button.attr('snd');
-              sounds[$snd].fx.play();
+            var playSound = function() {
+                var $button = $(this),
+                    $snd = $button.attr('snd');
+                sounds[$snd].fx.play();
             };
 
             $button.
             button().
             html($sfx.label).
-            attr({snd:s}).
+            attr({ snd: s }).
             click(playSound).
             appendTo(parentId).
             show();
         }
     }
 
-    makeButtons(sounds,'#everything');
-    makeButtons(secretSounds,'#secret');
+    $('#quote').button().css({
+        'font': 'inherit',
+        'color': 'inherit',
+        'text-align': 'left',
+        'outline': 'none',
+        'cursor': 'text',
+        'width': '50%'
+    });
+
+    $('#say').button().click(function() {
+        var quote = $('#quote').val();
+        say(quote);
+        
+    });
+
+    $('#clear').button().click(function() {
+        $('#quote').val('').focus();
+    });
+
+    $('#voiceName').val(voiceOptions.voiceName).change(function() {
+        voiceOptions.voiceName = $(this).val();
+        saveVoiceOptions();
+        
+    });
+
+    $('#voiceRateSlider').slider({
+        value: voiceOptions.rate,
+        min: 0.1,
+        max: 2,
+        step: 0.01,
+        slide: function(event, ui) {
+            voiceOptions.rate = ui.value;
+            $("#voiceRate").val(ui.value);
+            saveVoiceOptions();
+            
+        }
+    });
+
+    $('#rateLabel').addClass('small').button().click(function() {
+        voiceOptions.rate = 1;
+        $("#voiceRate").val(1);
+        $('#voiceRateSlider').slider({ value: 1 });
+        
+    });
+
+    $("#voiceRate").val(voiceOptions.rate);
+
+
+    $('#voicePitchSlider').slider({
+        value: voiceOptions.pitch,
+        min: 0.1,
+        max: 2,
+        step: 0.01,
+        slide: function(event, ui) {
+            voiceOptions.pitch = ui.value;
+            $("#voicePitch").val(ui.value);
+            saveVoiceOptions();
+            
+        }
+    });
+
+    $('#pitchLabel').button().click(function() {
+        voiceOptions.pitch = 1;
+        $("#voicePitch").val(1);
+        $('#voicePitchSlider').slider({ value: 1 });
+        
+    });
+
+    $("#voicePitch").val(voiceOptions.pitch);
+
+    $('#hideSecret').click(function(){
+        $('#secret').hide();
+        var prefs= {
+            showSecret : false,
+            voiceOptions : voiceOptions
+        };
+        
+        chrome.storage.sync.set(prefs, function() {
+            console.log('saving prefs:',prefs);
+        });
+    });
+
+    makeButtons(sounds, '#everything');
+    makeButtons(secretSounds, '#secret');
 
 });
